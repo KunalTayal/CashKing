4711e967150d1b9bc4e0bb4be2f7a04b22366cea
Kunal Tayal <76695156+KunalTayal@users.noreply.github.com>
2023-03-03 01:12:07 +0530

    Svg added and custom appbar developed

---
+++ lib/app/components/custom_appbar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final Widget child;
+  final double height;
+
+  const CustomAppBar({
+    super.key,
+    required this.child,
+    this.height = kToolbarHeight,
+  });
+
+  @override
+  Size get preferredSize => Size.fromHeight(height);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: preferredSize.height,
+      color: Colors.red,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: FractionalOffset.centerLeft,
+          end: FractionalOffset.centerRight,
+          colors: [
+            Color(0xFF1185D5),
+            Color(0xFF33C1EF),
+          ],
+        ),
+      ),
+      child: child,
+    );
+  }
+}
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:get/get.dart';
 
+import '../../../components/custom_appbar.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -9,9 +11,43 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
+      drawer: IconButton(
+        icon: SvgPicture.asset('assets/svg/drawer_icon.svg'),
+        splashRadius: 20,
+        onPressed: () => Scaffold.of(context).openDrawer(),
+      ),
+      appBar: CustomAppBar(
+        child: Column(
+          children: [
+            AppBar(
+              title: const Text(
+                "Hey Shubham",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              actions: [
+                ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: CircleAvatar(
+                    radius: 14.5,
+                    child: SvgPicture.asset('assets/svg/wallet_home_icon.svg'),
+                  ),
+                  label: const Text(
+                    '₹ 452',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+              ],
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+            ),
+          ],
+        ),
       ),
       body: const Center(
         child: Text(
