d9ad8f57aeb7013d7020f18d93975be80b28a994
Kunal Tayal <76695156+KunalTayal@users.noreply.github.com>
2023-03-03 00:21:49 +0530

    Custom Trending Offer card is done

--- lib/app/components/custom_trending_offer_card.dart
+++ lib/app/components/custom_trending_offer_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CustomTrendingOfferCard extends StatelessWidget {
   final Color color;
@@ -24,7 +25,52 @@ class CustomTrendingOfferCard extends StatelessWidget {
         color: color,
       ),
       child: Column(
-        children: const [],
+        children: [
+          SizedBox(
+            height: 127,
+            child: Image.network(
+              url,
+              fit: BoxFit.fill,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 12, top: 12),
+            child: Text(
+              title,
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 9, horizontal: 12),
+            child: Text(
+              "Get Rs. $amount",
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: Row(
+              children: [
+                SvgPicture.asset('assets/svg/bolt_sign.svg'),
+                const SizedBox(width: 7),
+                Text(
+                  "$lead users",
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 10,
+                    fontWeight: FontWeight.w500,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
